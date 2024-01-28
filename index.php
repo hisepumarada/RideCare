38ed3bdb411a1f238308e4d99b13867decb84505
hisepumarada <jcpumarada5@gmail.com>
2024-01-28 17:28:31 +0800

    title change

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include "db_conn.php";
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>RideCare: LOGIN Hello</title>
+    <title>RideCare: LOGIN JC</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/headers/">
     <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
