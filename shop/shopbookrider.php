a42ff59ff0f114092e0d17a270d634229c495770
hisepumarada <jcpumarada5@gmail.com>
2024-01-30 23:15:28 +0800

    title change

--- shop/shopbookrider.php
+++ shop/shopbookrider.php
@@ -19,7 +19,7 @@ $page = 'shopriders';
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
     <link href="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-1.13.8/b-2.4.2/b-html5-2.4.2/b-print-2.4.2/datatables.min.css" rel="stylesheet">
-	<title>RideCare SHOP: Payment</title>
+	<title>RideCare SHOP: Rider Appointments</title>
 
 <?php include "../inc/sidebarshop.php"; ?>
 </head>	
